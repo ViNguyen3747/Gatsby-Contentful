b86fe12ee27185bf2be08c1782dcc45db5851f7e
Thi Kieu Vi Nguyen <vinguyen1172000@gmail.com>
2021-10-07 22:24:55 -0500

    optimize performance

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "./src/components/Layout";
 
-export function wrapPageElement({ element }) {
+export function wrapRootElement({ element }) {
   return <Layout>{element}</Layout>;
 }
