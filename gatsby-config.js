02f70d394db644b40a5a57ea2ed2ce03031b1b22
Thi Kieu Vi Nguyen <vinguyen1172000@gmail.com>
2021-10-10 16:42:25 -0500

    optimization

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: `${__dirname}/static/images`,
+        path: `${__dirname}/src/images`,
       },
     },
     {
