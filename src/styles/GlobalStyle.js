904c2d10da0e219c00fdb71f8260188fdabf1b4d
Thi Kieu Vi Nguyen <vinguyen1172000@gmail.com>
2021-10-21 13:08:06 -0500

    optimized performance

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,7 +1,8 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
-
+  ${reset}
     html {
         box-sizing: border-box;
         margin: 0;
