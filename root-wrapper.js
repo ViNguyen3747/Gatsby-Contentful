11f0a3795a87e9cc224d45cabb35cdad23eb2292
Thi Kieu Vi Nguyen <vinguyen1172000@gmail.com>
2021-10-23 23:38:25 -0500

    minor update

--- root-wrapper.js
+++ root-wrapper.js
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "./src/components/Footer";
 import { GlobalStyle } from "./src/styles/GlobalStyle";
 
-export function wrapRootElement({ element }) {
+export function wrapPageElement({ element }) {
   return (
     <>
       <GlobalStyle />
